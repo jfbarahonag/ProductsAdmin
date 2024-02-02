4c16115f78b44e605a174c3b78b0d95c2876e034
jfbarahonag <jfbarahonag@unal.edu.co>
2024-02-01 23:17:20 -0500

    connection to api has been added

--- Program.cs
+++ Program.cs
@@ -6,6 +6,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var apiUrl = builder.Configuration.GetValue<string>("apiUrl");
+
+if (string.IsNullOrEmpty(apiUrl))
+{
+  throw new Exception("apiUrl is null or empty");
+}
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
 
 await builder.Build().RunAsync();
